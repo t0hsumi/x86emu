53bdfef69d98b506569d38344e22b726d1231b5e
Taito Ohsumi <ohsumi-taito035@g.ecc.u-tokyo.ac.jp>
2023-06-12 11:23:19 +0900

    [insctuction.c] use helper function instead of operating register directly.

--- instruction.c
+++ instruction.c
@@ -17,7 +17,7 @@ instruction_func_t *instructions[256];
 static void mov_r32_imm32(Emulator *emu) {
   uint8_t reg = get_code8(emu, 0) - 0xB8;
   uint32_t value = get_code32(emu, 1);
-  emu->registers[reg] = value;
+  set_register32(emu, reg, value);
   emu->eip += 5;
 }
 
